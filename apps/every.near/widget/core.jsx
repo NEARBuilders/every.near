3718be08013e4c8e5682d89a3e5d2ba6f1029d19
Elliot Braem <16282460+elliotBraem@users.noreply.github.com>
2024-01-27 19:54:52 -0500

    bos-workspace clone

---
+++ apps/every.near/widget/core.jsx
@@ -0,0 +1,7 @@
+const data = JSON.parse(
+  Social.get("efiz.near/thing/routes", "final") || "null"
+);
+
+return (
+  <Widget src={"every.near/widget/app"} props={{ data, path: "", ...props }} />
+);
